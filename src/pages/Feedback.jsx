0fd9846e241caed33854af70b3282d6da058b33b
NabilF <nabilbkl731@gmail.com>
2026-04-17 22:56:51 +0700

    Delete src directory

--- src/pages/Feedback.jsx
+++
@@ -1,8 +0,0 @@
-export default function Feedback() {
-  return (
-    <div className="page">
-      <h1>Feedback 💬</h1>
-      <p>Email: jjgame@email.com</p>
-    </div>
-  );
-}
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="page">
-      <h1>Welcome to JJ Game 🎮</h1>
-      <p>Game misteri 2D penuh teka-teki.</p>
-      <button className="btn">Play Now</button>
-    </div>
-  );
-}
